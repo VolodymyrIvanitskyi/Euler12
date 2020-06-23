782e7c8283462a296fa63f7554ef0810ada34481
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-06-23 22:22:50 +0300

    Euler 12 is completed

--- Euler12/Program.cs
+++ Euler12/Program.cs
@@ -6,7 +6,43 @@ namespace Euler12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool flag = true;
+            int k = 10;
+            int triangularNumber;
+
+            while(flag)
+            {
+                triangularNumber = TriangularNumber(k); // знаходимо трикутне число
+                if(NumbersOfDivisors(triangularNumber)>=200) // шукаємо число кількість дільників якого перевищує 200
+                {
+                    Console.WriteLine($"TriangleNumber = {triangularNumber}, numberOfDivisors = {NumbersOfDivisors(triangularNumber)}");
+                    
+                    flag = false;
+
+                }
+
+
+                ++k;
+            }
+        }
+        
+        static int NumbersOfDivisors(int number)
+        {
+            int quantity = 0;
+            for (int i = 1; i <= number; i++)
+                if (number % i == 0)
+                    quantity++;
+            return quantity;
+        }
+        static int  TriangularNumber(int number)
+        {
+            int sum=0;
+            for(int i = 1; i <= number; i++)
+            {
+                sum += i;
+            }
+            
+            return sum;
         }
     }
 }
